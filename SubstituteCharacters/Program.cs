e5e6ee26f4edb8048e870eba11c71ed210ff5776
Robi <sikkelh@gmail.com>
2021-10-12 15:30:04 +0300

    More Strings Exercises

---
+++ SubstituteCharacters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SubstituteCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestda nime ja perekonnanime
+            //programm asendab kõik 'a' tähed kasutaja nimes tärniga (*)
+            //programm kuvab tulemust konsoolis
+
+            Console.WriteLine("Sisesta eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Sisesta perekonnanimi:");
+            string lastName = Console.ReadLine();
+            string fullName = $"{firstName} {lastName}";
+
+            fullName = fullName.Replace('a', '*');
+
+            Console.WriteLine(fullName);
+        }
+    }
+}
---
+++ WordCounter/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WordCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab, mitu sõna on lauses "Hello World!"
+
+            string helloW = "Hello World!";
+
+            //while
+
+            //int i = 0;
+            //int wordCounter = 1;
+
+            /*while(i < helloW.Length)
+            {
+                if(helloW[i] == ' ')
+                {
+                    wordCounter++;
+                }
+                i++;
+            }*/
+            int wordCounter = 1;
+            for (int i = 0; i < helloW.Length; i++)
+            {
+                if (helloW[i] == ' ')
+                {
+                    wordCounter++;
+                }
+            }
+
+            Console.WriteLine($"Lauses {helloW} on {wordCounter} sõna");
+
+
+        }
+    }
+    
+}
